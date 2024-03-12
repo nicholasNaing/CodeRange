4ffe331ca872d57f058d80deeaf916afed2175b5
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2024-03-12 16:55:30 +0700

    added nav hover effects and background video

--- CodeRange/src/layouts/NavBar.jsx
+++ CodeRange/src/layouts/NavBar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className={`flex flex-row justify-between items-center w-screen min-h-5 py-3 absolute text-slate-50 bg-black bg-opacity-30`}>
+    <div className={`flex flex-row justify-between items-center w-screen min-h-5 py-3 absolute text-slate-50 bg-black bg-opacity-40`}>
         <div className={`flex-1 nav-logo text-xl font-bold pl-3`}>
             Code Range
         </div>
-        <div className={`flex-1 flex justify-evenly`}>
-            <div>Home</div>
-            <div>Projects</div>
-            <div>Services</div>
-            <div>Expertise</div>
-            <div>Community</div>
-            <div>search</div>
+        <div className={`nav-items flex-1 flex justify-evenly text-lg`}>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>Home</div>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>Projects</div>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>Services</div>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>Expertise</div>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>Community</div>
+            <div className={`hover:text-emerald-400 hover:text-2xl hover:line-through cursor-default duration-500`}>search</div>
         </div>
     </div>
   )
--- CodeRange/src/view/BannerPage.jsx
+++ CodeRange/src/view/BannerPage.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import CRLOGO from "../assets/codeRangeLogo.png"
+import backgroundCR from "../assets/backgroundCR.mp4"
 import "../fonts/Roxborough-CF/RoxboroughCF.ttf"
 
 function BannerPage() {
   return (
-    <div className={`min-h-screen min-w-screen bg-slate-500 lg:flex`}>
-        <div className={`bg-black flex-1 flex flex-col pt-36 items-center`}>
+    <div className={`min-h-screen min-w-screen bg-slate-900 bg-opacity-40 lg:flex`}>
+        <video className="absolute top-0 left-0 w-full h-full object-cover -z-10" autoPlay muted loop>
+            <source src={backgroundCR} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+        <div className={`flex-1 flex flex-col pt-36 items-center`}>
             <div className={`flex`}>
-                <img src={CRLOGO} alt="This is the logo of codeRange" width={120} height={120}/>
+                <img className={`hover:animate-spin`} src={CRLOGO} alt="This is the logo of codeRange" width={120} height={120}/>
                 <div className={`code-range font-bold text-white text-[70px] tracking-wide px-4`}>|</div>
                 <div className={`code-range text-white text-[70px] tracking-wide`}>Code Range</div>
             </div>
-            <div className={`intro-text text-white text-[25px] tracking-wider pl-3`}>Breaking rules, Building codes</div>
+            <div className={`intro-text hover:animate-ping text-white text-[25px] tracking-wider pl-3`}>Breaking rules, Building codes</div>
         </div>
-        <div className={`bg-slate-700 flex-1`}>
-            World
+        <div className={`flex-1`}>
         </div>
     </div>
   )
